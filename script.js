eb89b28a7a3cd3de9de6575011a90560ff81f71d
Roman <78878023+coldshein@users.noreply.github.com>
2022-04-05 13:29:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,32 +10,6 @@ links.forEach(el=>{
     
 })
 
-const bubbles = document.querySelectorAll(".bubbly");
-let index = 0;
-function active(e){
-  for (bubble of bubbles){
-    bubble.style.opacity=".7";
-    bubble.style.backgroundColor="gray";
-    bubble.style.boxShadow="";
-
-  }
-  bubbles[e].style.opacity="1";
-  bubbles[e].style.backgroundColor="red";
-  bubbles[e].style.boxShadow="0 0 40px 10px red";
-}
-
-function nextRound(){
-  if(index == bubbles.length - 1){
-    index = 0;
-    active(index);
-  }
-  else{
-    index++;
-    active(index);
-  }
-}
-setInterval(nextRound, 2000);
-
 // BURGER
 const burger = document.querySelector('.burger-menu');
 const burgerNav = document.querySelector('.burger-nav');
@@ -56,6 +30,20 @@ burgerLink.forEach(el => {
 });
 
 
+VANTA.TRUNK({
+  el: "#home",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 0.00,
+  scaleMobile: 0.00,
+  color: 0xDB1919,
+  backgroundColor: 0x1D1D1D,
+  spacing: 0.00,
+  chaos: 6.00
+})
 
 
 
